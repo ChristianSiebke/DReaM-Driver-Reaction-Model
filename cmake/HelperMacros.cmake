1fd8ae9eb0d27208aa1fd6ef2c31eeee12dfa31d
hpcwoess <woessner@hlrs.de>
2020-03-02 12:29:34 +0100

    added copyright
    Signed-off-by: hpcwoess <woessner@hlrs.de>

--- cmake/HelperMacros.cmake
+++ cmake/HelperMacros.cmake
@@ -1,6 +1,12 @@
-# @file OPENPASSHelperMacros.cmake
-#
-# @author Blasius Czink
+#/*******************************************************************************
+#* Copyright (c) 2020 HLRS, University of Stuttgart
+#*
+#* This program and the accompanying materials are made
+#* available under the terms of the Eclipse Public License 2.0
+#* which is available at https://www.eclipse.org/legal/epl-2.0/
+#*
+#* SPDX-License-Identifier: EPL-2.0
+#*******************************************************************************/
 #
 # Provides helper macros for build control:
 #
