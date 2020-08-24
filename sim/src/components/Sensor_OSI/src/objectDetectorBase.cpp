971ed018ca0592a6e25af880036e6e8103cb8c3b
Weiss David <david.weiss@in-tech.com>
2020-08-24 15:56:26 +0200

    mod(Sensor_OSI): Move methods to common base

--- sim/src/components/Sensor_OSI/src/objectDetectorBase.cpp
+++ sim/src/components/Sensor_OSI/src/objectDetectorBase.cpp
@@ -104,6 +104,45 @@ void ObjectDetectorBase::UpdateOutput(int localLinkId, std::shared_ptr<SignalInt
     }
 }
 
+void ObjectDetectorBase::AddMovingObjectToSensorData(osi3::MovingObject object, point_t ownVelocity, point_t ownAcceleration, point_t ownPosition, double yaw, double yawRate)
+{
+    point_t objectReferencePointGlobal{object.base().position().x(), object.base().position().y()};
+    point_t objectReferencePointLocal = TransformPointToLocalCoordinates(objectReferencePointGlobal, ownPosition, yaw);
+
+    osi3::DetectedMovingObject* detectedObject = sensorData.add_moving_object();
+    detectedObject->mutable_header()->add_ground_truth_id()->set_value(object.id().value());
+    detectedObject->mutable_header()->add_sensor_id()->set_value(id);
+    detectedObject->mutable_base()->mutable_dimension()->CopyFrom(object.base().dimension());
+
+    detectedObject->mutable_base()->mutable_position()->set_x(objectReferencePointLocal.x());
+    detectedObject->mutable_base()->mutable_position()->set_y(objectReferencePointLocal.y());
+    detectedObject->mutable_base()->mutable_orientation()->set_yaw(object.base().orientation().yaw() - yaw);
+    detectedObject->mutable_base()->mutable_orientation_rate()->set_yaw(object.base().orientation_rate().yaw() - yawRate);
+    point_t objectVelocity{object.base().velocity().x(), object.base().velocity().y()};
+    point_t relativeVelocity = CalculateRelativeVector(objectVelocity, ownVelocity, yaw);
+    detectedObject->mutable_base()->mutable_velocity()->set_x(relativeVelocity.x());
+    detectedObject->mutable_base()->mutable_velocity()->set_y(relativeVelocity.y());
+    point_t objectAcceleration{object.base().acceleration().x(), object.base().acceleration().y()};
+    point_t relativeAcceleration = CalculateRelativeVector(objectAcceleration, ownAcceleration, yaw);
+    detectedObject->mutable_base()->mutable_acceleration()->set_x(relativeAcceleration.x());
+    detectedObject->mutable_base()->mutable_acceleration()->set_y(relativeAcceleration.y());
+}
+
+void ObjectDetectorBase::AddStationaryObjectToSensorData(osi3::StationaryObject object, point_t ownPosition, double yaw)
+{
+    point_t objectReferencePointGlobal{object.base().position().x(), object.base().position().y()};
+    point_t objectReferencePointLocal = TransformPointToLocalCoordinates(objectReferencePointGlobal, ownPosition, yaw);
+
+    osi3::DetectedStationaryObject* detectedObject = sensorData.add_stationary_object();
+    detectedObject->mutable_header()->add_ground_truth_id()->set_value(object.id().value());
+    detectedObject->mutable_header()->add_sensor_id()->set_value(id);
+    detectedObject->mutable_base()->mutable_dimension()->CopyFrom(object.base().dimension());
+
+    detectedObject->mutable_base()->mutable_position()->set_x(objectReferencePointLocal.x());
+    detectedObject->mutable_base()->mutable_position()->set_y(objectReferencePointLocal.y());
+    detectedObject->mutable_base()->mutable_orientation()->set_yaw(object.base().orientation().yaw() - yaw);
+}
+
 Position ObjectDetectorBase::GetAbsolutePosition()
 {
     Position absolutePosition;
@@ -268,4 +307,34 @@ void ObjectDetectorBase::ParseBasicParameter()
     position.pitch = doubleParameters.at("Pitch");
     position.yaw = doubleParameters.at("Yaw");
     position.roll = doubleParameters.at("Roll");
+}
+
+const osi3::MovingObject* ObjectDetectorBase::FindHostVehicleInSensorView(const osi3::SensorView& sensorView)
+{
+    const auto hostVehicleIt = std::find_if(sensorView.global_ground_truth().moving_object().cbegin(),
+                                            sensorView.global_ground_truth().moving_object().cend(),
+                                            [&sensorView](const osi3::MovingObject& object)
+                                            {
+                                                return object.id().value() == sensorView.host_vehicle_id().value();
+                                            });
+
+    if (hostVehicleIt == sensorView.global_ground_truth().moving_object().cend())
+    {
+        throw std::runtime_error("Host vehicle not in SensorView");
+    }
+
+    return &(*hostVehicleIt);
+}
+
+point_t ObjectDetectorBase::GetHostVehiclePosition(const osi3::MovingObject* hostVehicle) const
+{
+    point_t bbCenterToRear{hostVehicle->vehicle_attributes().bbcenter_to_rear().x(), hostVehicle->vehicle_attributes().bbcenter_to_rear().y()};
+    bt::rotate_transformer<bg::radian, double, 2, 2> rotate(-hostVehicle->base().orientation().yaw());
+    bt::translate_transformer<double, 2, 2> bbCenter(hostVehicle->base().position().x(), hostVehicle->base().position().y());
+    point_t rotatedBbCenterToRear;
+    point_t ownPosition;
+    bg::transform(bbCenterToRear, rotatedBbCenterToRear, rotate);
+    bg::transform(rotatedBbCenterToRear, ownPosition, bbCenter);
+
+    return ownPosition;
 }
