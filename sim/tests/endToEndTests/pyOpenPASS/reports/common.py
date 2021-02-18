36853bb375d0887094c3439abc422969c00b2200
Reinhard Biegel <reinhard.biegel@in-tech.com>
2021-02-18 17:19:04 +0100

    fix(pyOpenPASS): Add copyright headers

--- sim/tests/endToEndTests/pyOpenPASS/reports/common.py
+++ sim/tests/endToEndTests/pyOpenPASS/reports/common.py
@@ -1,5 +1,14 @@
-from pathlib import Path
+################################################################################
+# Copyright (c) 2020, 2021 in-tech GmbH
+#
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+################################################################################
 
+from pathlib import Path
 
 class Report:
     def __init__(self, output_path, all_config_dirs):
@@ -18,4 +27,3 @@ class Report:
     def generate(self):
         self.generated = str(self)
         Path(self.output_path).mkdir(parents=True, exist_ok=True)
-
