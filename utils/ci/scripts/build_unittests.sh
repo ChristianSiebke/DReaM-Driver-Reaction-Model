34667336ff16a84f68dbad236fd0d71c30b15187
Reinhard Biegel <reinhard.biegel@in-tech.com>
2021-03-24 01:22:08 +0100

    mod(CI): Parallelize core build and unit testing
    Signed-off-by: Reinhard Biegel <reinhard.biegel@in-tech.com>

---
+++ utils/ci/scripts/build_unittests.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+################################################################################
+# Copyright (c) 2021 in-tech GmbH
+#
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+################################################################################
+
+################################################################################
+# This script runs the simulation core unit tests
+################################################################################
+
+MYDIR="$(dirname "$(readlink -f $0)")"
+cd "$MYDIR" || exit 1
+
+export CCACHE_DIR=$MYDIR/../../../../ccache
+
+for SCRIPT in 10_prepare.sh 15_dep_osi.sh 16_dep_fmilibrary.sh 20_configure.sh 50_test.sh; do
+  echo
+  echo "======================================================================="
+  echo "Executing ${SCRIPT}..."
+  echo "======================================================================="
+  echo
+
+  ./$SCRIPT || exit 1
+done
+
