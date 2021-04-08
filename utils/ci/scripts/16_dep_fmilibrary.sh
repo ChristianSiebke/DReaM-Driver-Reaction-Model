47a673c2872fcd121b4fa02cabc2645d2b487e1a
Reinhard Biegel <reinhard.biegel@in-tech.com>
2021-04-08 17:05:28 +0200

    mod(CI): Attempt to fix EOL problems with FMILibrary sources
    Signed-off-by: Reinhard Biegel <reinhard.biegel@in-tech.com>

--- utils/ci/scripts/16_dep_fmilibrary.sh
+++ utils/ci/scripts/16_dep_fmilibrary.sh
@@ -24,7 +24,7 @@ mkdir -p build-fmi-library && cd build-fmi-library
 git clone --branch 2.0.3 --depth=1 -c advice.detachedHead=false https://github.com/modelon-community/fmi-library.git src || exit 1
 
 cd src
-patch -p1 < "$MYDIR/patches/fmi-library-2.0.3-fixes.patch" || exit 1
+patch -l -p1 < "$MYDIR/patches/fmi-library-2.0.3-fixes.patch" || exit 1
 
 mkdir -p build && cd build
 cmake -D CMAKE_BUILD_TYPE=Release -D FMILIB_INSTALL_PREFIX="$WORKSPACE_ROOT/deps/FMILibrary" -D FMILIB_BUILD_STATIC_LIB=OFF -D FMILIB_BUILD_SHARED_LIB=ON ..
