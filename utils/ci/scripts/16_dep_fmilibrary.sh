ca24307a413caa25c7c3a6a1e690fce5c13b6387
Reinhard Biegel <reinhard.biegel@in-tech.com>
2021-04-29 15:30:15 +0200

    mod(CI): Don't build FMILibrary tests
    Signed-off-by: Reinhard Biegel <reinhard.biegel@in-tech.com>

--- utils/ci/scripts/16_dep_fmilibrary.sh
+++ utils/ci/scripts/16_dep_fmilibrary.sh
@@ -33,7 +33,7 @@ dos2unix src/Import/src/FMI1/fmi1_import_capi.c src/Import/src/FMI2/fmi2_import_
 patch -l -p1 < "$MYDIR/patches/fmi-library-2.0.3-fixes.patch" || exit 1
 
 mkdir -p build && cd build
-cmake "$CMAKE_GENERATOR_ARG" -D CMAKE_BUILD_TYPE=Release -D FMILIB_INSTALL_PREFIX="$WORKSPACE_ROOT/deps/FMILibrary" -D FMILIB_BUILD_STATIC_LIB=OFF -D FMILIB_BUILD_SHARED_LIB=ON ..
+cmake "$CMAKE_GENERATOR_ARG" -D CMAKE_BUILD_TYPE=Release -D FMILIB_INSTALL_PREFIX="$WORKSPACE_ROOT/deps/FMILibrary" -D FMILIB_BUILD_STATIC_LIB=OFF -D FMILIB_BUILD_SHARED_LIB=ON -D FMILIB_BUILD_TESTS=OFF ..
 make -j4
 make install
 
