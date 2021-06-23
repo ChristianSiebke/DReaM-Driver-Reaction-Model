775b67184c5b50caa3650f48f7f62ca851f71adb
mscharfenberg <Michael.Scharfenberg@itk-engineering.de>
2021-06-23 17:19:27 +0200

    Update confile for packaging openpass

--- utils/ci/conan/recipe/openpass/conanfile.py
+++ utils/ci/conan/recipe/openpass/conanfile.py
@@ -46,14 +46,11 @@ class OpenpassConan(ConanFile):
         cmake.build()
 
     def package(self):
-        self.copy("*.h", dst="include", src="openpass")
         self.copy("*.exe", dst="bin", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin/lib", keep_path=False)
+        self.copy("*.a", dst="bin/lib", keep_path=False)
+        self.copy("*.so", dst="bin/lib", keep_path=False)
         self.copy("*.html", dst="doc")
 
     def package_info(self):
         self.cpp_info.libs = ["openpass"]
-
