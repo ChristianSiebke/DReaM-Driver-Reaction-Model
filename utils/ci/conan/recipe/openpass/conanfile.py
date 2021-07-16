b5cabf11134437f4f6b6eea1cc93c9c4c6c7f2e4
mscharfenberg <Michael.Scharfenberg@itk-engineering.de>
2021-07-16 17:30:49 +0200

    Add copyright Header to py files

--- utils/ci/conan/recipe/openpass/conanfile.py
+++ utils/ci/conan/recipe/openpass/conanfile.py
@@ -1,5 +1,18 @@
-from conans import ConanFile, CMake, tools
+################################################################################
+# Copyright (c) 2021 ITK Engineering GmbH
+#
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+################################################################################
 
+################################################################################
+# Install file for building OpenPASS with Conan
+################################################################################
+
+from conans import ConanFile, CMake, tools
 
 class OpenpassConan(ConanFile):
     name = "openpass"
