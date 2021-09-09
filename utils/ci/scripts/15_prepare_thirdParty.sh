053b0753d2b5f13f62da2a2524c8c2d2c6f6924e
mscharfenberg <Michael.Scharfenberg@itk-engineering.de>
2021-09-09 07:53:10 +0200

    Update conan intall script with python call

--- utils/ci/scripts/15_prepare_thirdParty.sh
+++ utils/ci/scripts/15_prepare_thirdParty.sh
@@ -14,18 +14,21 @@
 # This script prepares the thirdParty dependencies
 ################################################################################
 
+MYDIR="$(dirname "$(readlink -f $0)")"
+cd "$MYDIR/../../../.."
+
 WORKSPACE_ROOT="$PWD"
 
 # Detect system settings and create a conan profile
-conan profile new default --detect
-conan profile update settings.compiler.libcxx=libstdc++11 default
+python -m conans.conan profile new default --detect
+python -m conans.conan profile update settings.compiler.libcxx=libstdc++11 default
 
 # export conan recipes of thirdParty dependencies to conans local cache
-conan export $WORKSPACE_ROOT/utils/ci/conan/recipe/osi openpass/testing
-conan export $WORKSPACE_ROOT/utils/ci/conan/recipe/fmi openpass/testing
+python -m conans.conan export $WORKSPACE_ROOT/utils/ci/conan/recipe/osi openpass/testing
+python -m conans.conan export $WORKSPACE_ROOT/utils/ci/conan/recipe/fmi openpass/testing
 
 # prepare a build directory
 mkdir -p build-thirdParty && cd build-thirdParty
 
 # (build and) install thirdParty dependencies to conans local cache and deploy them
-conan install $WORKSPACE_ROOT/utils/ci/conan/install_file/thirdParty --build=missing --install-folder="$WORKSPACE_ROOT/deps/" -g deploy
+python -m conans.conan install $WORKSPACE_ROOT/utils/ci/conan/install_file/thirdParty --build=missing --install-folder="$WORKSPACE_ROOT/deps/" -g deploy
