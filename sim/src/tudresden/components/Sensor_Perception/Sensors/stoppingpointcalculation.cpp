c4f18262dcbdbf5f3c164f2341e1e3f89a37a782
CGaerber <cgaerber@mailbox.tu-dresden.de>
2022-08-01 22:00:07 +0200

    stopping point fix

--- sim/src/tudresden/components/Sensor_Perception/Sensors/stoppingpointcalculation.cpp
+++ sim/src/tudresden/components/Sensor_Perception/Sensors/stoppingpointcalculation.cpp
@@ -8,22 +8,36 @@ Line2d StoppingPointCalculation::CalcExtendedLine(const MentalInfrastructure::La
     bool rightWay = (roadIsPredJunction && lane->IsInRoadDirection()) || (roadIsSuccJunction && !lane->IsInRoadDirection());
     auto pointsList = lane->GetLanePoints();
     auto secondLast = [](bool rightWay, auto pointsList) {
-        return rightWay ? std::next(pointsList.begin(), 2) : std::prev(pointsList.end(), 2);
+        return rightWay ? std::next(pointsList.begin(), 1) : std::prev(pointsList.end(), 2);
     };
     auto last = [](bool rightWay, auto lane) { return rightWay ? lane->GetFirstPoint() : lane->GetLastPoint(); };
 
     laneDirection.x = last(rightWay, lane)->x - (*secondLast(rightWay, pointsList)).x;
     laneDirection.y = last(rightWay, lane)->y - (*secondLast(rightWay, pointsList)).y;
 
+    // std::cout << "Vec calc: " << laneDirection << " = "
+    //           << "(" << last(rightWay, lane)->x << "," << last(rightWay, lane)->y << ") - (" << (*secondLast(rightWay, pointsList)).x <<
+    //           ","
+    //           << (*secondLast(rightWay, pointsList)).y << ")" << std::endl;
+    // std::cout << "Vec before: " << laneDirection << std::endl;
+
     lineStart.x = last(rightWay, lane)->x;
     lineStart.y = last(rightWay, lane)->y;
 
-    Common::Vector2d copy = laneDirection;
+    double xCpy = laneDirection.x;
+    double yCpy = laneDirection.y;
+
+    Common::Vector2d copy(xCpy, yCpy);
     copy.Norm();
     copy.Scale(lane->GetWidth() / 2);
+
+    // lane->IsInRoadDirection() ? rotationAngle *= 1 : rotationAngle *= -1;
+
     copy.Rotate(rotationAngle);
 
     lineStart.Add(copy);
+
+    // std::cout << "Vec after: " << laneDirection << std::endl;
 
     Line2d line;
     line.start = lineStart;
@@ -98,6 +112,20 @@ bool StoppingPointCalculation::CalcCrossingLines(const MentalInfrastructure::Lan
         lines.innerRightLine = CalcExtendedLine(irl, -M_PI_2, roadIsPredJunction, roadIsSuccJunction);
         lines.centerLine = CalcExtendedLine(centerRightLane, centerAngle, roadIsPredJunction, roadIsSuccJunction);
 
+        std::cout << "Road=" << incomingRoad->GetOpenDriveId() << "\t"
+                  << "AppDir=" << std::to_string((int)dir) << std::endl
+                  << "Outer Right: \t Ray((" << lines.outerRightLine.start.x << "," << lines.outerRightLine.start.y << "),("
+                  << lines.outerRightLine.direction.x << "," << lines.outerRightLine.direction.y << "))" << std::endl
+                  << "Inner Right: \t Ray((" << lines.innerRightLine.start.x << "," << lines.innerRightLine.start.y << "),("
+                  << lines.innerRightLine.direction.x << "," << lines.innerRightLine.direction.y << "))" << std::endl
+                  << "Center: \t Ray((" << lines.centerLine.start.x << "," << lines.centerLine.start.y << "),("
+                  << lines.centerLine.direction.x << "," << lines.centerLine.direction.y << "))" << std::endl
+                  << "Inner Left: \t Ray((" << lines.innerLeftLine.start.x << "," << lines.innerLeftLine.start.y << "),("
+                  << lines.innerLeftLine.direction.x << "," << lines.innerLeftLine.direction.y << "))" << std::endl
+                  << "Outer Left: \t Ray((" << lines.outerLeftLine.start.x << "," << lines.outerLeftLine.start.y << "),("
+                  << lines.outerLeftLine.direction.x << "," << lines.outerLeftLine.direction.y << "))" << std::endl
+                  << std::endl;
+
         return true;
     }
     else {
@@ -109,6 +137,7 @@ bool StoppingPointCalculation::CCLFromTurningLanes(const MentalInfrastructure::L
                                                    const MentalInfrastructure::Junction *junction) {
     if (lane->GetSuccessors().size() == 1) {
         const auto succLane = lane->GetSuccessors().front();
+        // std::cout << "Lane " << lane->GetRoad()->GetOpenDriveId() << " is in direction? " << lane->IsInRoadDirection() << std::endl;
         return CalcCrossingLines(succLane, dir, lines, junction);
     }
 
@@ -138,6 +167,32 @@ bool StoppingPointCalculation::InsertCrossLines(const std::vector<const MentalIn
     return true;
 }
 
+// https://www.geeksforgeeks.org/program-for-point-of-intersection-of-two-lines/
+Common::Vector2d lineLineIntersection(Common::Vector2d A, Common::Vector2d B, Common::Vector2d C, Common::Vector2d D) {
+    // Line AB represented as a1x + b1y = c1
+    double a1 = B.y - A.y;
+    double b1 = A.x - B.x;
+    double c1 = a1 * (A.x) + b1 * (A.y);
+
+    // Line CD represented as a2x + b2y = c2
+    double a2 = D.y - C.y;
+    double b2 = C.x - D.x;
+    double c2 = a2 * (C.x) + b2 * (C.y);
+
+    double determinant = a1 * b2 - a2 * b1;
+
+    if (determinant == 0) {
+        // The lines are parallel. This is simplified
+        // by returning a pair of FLT_MAX
+        return Common::Vector2d(FLT_MAX, FLT_MAX);
+    }
+    else {
+        double x = (b2 * c1 - b1 * c2) / determinant;
+        double y = (a1 * c2 - a2 * c1) / determinant;
+        return Common::Vector2d(x, y);
+    }
+}
+
 StoppingPoint StoppingPointCalculation::CalculateStoppingPoint(const MentalInfrastructure::Road *road,
                                                                const MentalInfrastructure::Lane *lane, Line2d line,
                                                                StoppingPointType type) {
@@ -148,6 +203,7 @@ StoppingPoint StoppingPointCalculation::CalculateStoppingPoint(const MentalInfra
     double minDistance = static_cast<double>(INFINITY);
 
     bool minPointNotAtEnd = false;
+    bool minPointAtNext = false;
 
     // loop over all points on ego lane
     // determine point with minimum distance
@@ -172,11 +228,13 @@ StoppingPoint StoppingPointCalculation::CalculateStoppingPoint(const MentalInfra
                 prev = {0.0, 0.0, 0.0, 0.0};
                 next = *std::next(it);
                 minPointNotAtEnd = false;
+                minPointAtNext = false;
             }
             else if (it != lane->GetLanePoints().begin()) {
                 prev = *std::prev(it);
                 next = {0.0, 0.0, 0.0, 0.0};
                 minPointNotAtEnd = false;
+                minPointAtNext = false;
             }
         }
     }
@@ -184,29 +242,79 @@ StoppingPoint StoppingPointCalculation::CalculateStoppingPoint(const MentalInfra
     Common::Vector2d origin{minEgoPoint.x, minEgoPoint.y};
     Common::Vector2d dirF{next.x, next.y};
     Common::Vector2d dirB{prev.x, prev.y};
-    dirF.Sub(origin);
-    dirB.Sub(origin);
 
-    Line2d forward;
-    Line2d backward;
-    forward.start = origin;
-    backward.start = origin;
-    forward.direction = dirF;
-    backward.direction = dirB;
+    if (minPointNotAtEnd) {
+        Common::Vector2d pointF = lineLineIntersection(origin, dirF, line.start, line.direction + line.start);
+        Common::Vector2d pointB = lineLineIntersection(origin, dirB, line.start, line.direction + line.start);
 
-    double offsetF = line.intersect(forward);
-    double offsetB = line.intersect(backward);
+        Common::Vector2d distF = origin - pointF;
+        Common::Vector2d distB = origin - pointB;
 
-    if (offsetB >= 0 && offsetB <= 1) {
-        minEgoPoint.x = backward.start.x + offsetB * backward.direction.x;
-        minEgoPoint.y = backward.start.y + offsetB * backward.direction.y;
-        minEgoPoint.sOffset = minEgoPoint.sOffset + offsetB * (prev.sOffset - minEgoPoint.sOffset);
+        double dF = distF.Length();
+        double dB = distB.Length();
+
+        if (dF < dB) {
+            minEgoPoint.x = pointF.x;
+            minEgoPoint.y = pointF.y;
+            double offsetF = dF / (origin - dirF).Length();
+
+            minEgoPoint.sOffset = minEgoPoint.sOffset + offsetF * (next.sOffset - minEgoPoint.sOffset);
+        }
+        else {
+            minEgoPoint.x = pointB.x;
+            minEgoPoint.y = pointB.y;
+            double offsetB = dB / (origin - dirB).Length();
+
+            minEgoPoint.sOffset = minEgoPoint.sOffset + offsetB * (prev.sOffset - minEgoPoint.sOffset);
+        }
     }
-    else if (offsetF >= 0 && offsetF <= 1) {
-        minEgoPoint.x = forward.start.x + offsetF * forward.direction.x;
-        minEgoPoint.y = forward.start.y + offsetF * forward.direction.y;
-        minEgoPoint.sOffset = minEgoPoint.sOffset + offsetF * (next.sOffset - minEgoPoint.sOffset);
-    }
+
+    // Common::Vector2d dirF{next.x - minEgoPoint.x, next.y - minEgoPoint.y};
+    // Common::Vector2d dirB{prev.x - minEgoPoint.x, prev.y - minEgoPoint.y};
+
+    // Line2d forward;
+    // Line2d backward;
+    // forward.start = origin;
+    // backward.start = origin;
+    // forward.direction = dirF;
+    // backward.direction = dirB;
+
+    // if (minPointNotAtEnd) {
+    //     double offsetF = line.intersect(forward);
+    //     double offsetB = line.intersect(backward);
+
+    //     if (offsetB >= -1 && offsetB <= 1) {
+    //         minEgoPoint.x = origin.x + offsetB * dirB.x;
+    //         minEgoPoint.y = origin.y + offsetB * dirB.y;
+    //         minEgoPoint.sOffset = minEgoPoint.sOffset + offsetB * (prev.sOffset - minEgoPoint.sOffset);
+    //     }
+    //     else if (offsetF >= -1 && offsetF <= 1) {
+    //         minEgoPoint.x = origin.x + offsetF * dirF.x;
+    //         minEgoPoint.y = origin.y + offsetF * dirF.y;
+    //         minEgoPoint.sOffset = minEgoPoint.sOffset + offsetF * (next.sOffset - minEgoPoint.sOffset);
+    //     }
+    // }
+    // else {
+    //     double offset = 0;
+    //     Common::Vector2d dir;
+    //     double sOffset;
+    //     if (minPointAtNext) {
+    //         offset = line.intersect(backward);
+    //         dir = dirB;
+    //         sOffset = prev.sOffset;
+    //     }
+    //     else {
+    //         offset = line.intersect(forward);
+    //         dir = dirF;
+    //         sOffset = next.sOffset;
+    //     }
+
+    //     if (offset != 0) {
+    //         minEgoPoint.x = origin.x + offset * dir.x;
+    //         minEgoPoint.y = origin.y + offset * dir.y;
+    //         minEgoPoint.sOffset = minEgoPoint.sOffset + offset * (sOffset - minEgoPoint.sOffset);
+    //     }
+    // }
 
     // setting the struct
     stoppingPoint.type = type;
@@ -297,6 +405,17 @@ StoppingPointCalculation::DetermineStoppingPoints(const MentalInfrastructure::Ju
     InsertCrossLines(nextLanes.leftLanes, crossLineMap, junction, leftSuccLane, dummyCrossLines, ApproachDirection::Left);
     InsertCrossLines(nextLanes.straightLanes, crossLineMap, junction, straightSuccLane, dummyCrossLines, ApproachDirection::StraightAhead);
     InsertCrossLines(nextLanes.rightLanes, crossLineMap, junction, rightSuccLane, dummyCrossLines, ApproachDirection::Right);
+
+    // std::cout << "EGO: " << egoLane->GetRoad()->GetOpenDriveId() << std::endl;
+    // for (auto lane : nextLanes.leftLanes)
+    //     std::cout << "Left: " << lane->GetRoad()->GetOpenDriveId() << std::endl;
+    // for (auto lane : nextLanes.straightLanes)
+    //     std::cout << "Straight: " << lane->GetRoad()->GetOpenDriveId() << std::endl;
+    // for (auto lane : nextLanes.rightLanes)
+    //     std::cout << "Right: " << lane->GetRoad()->GetOpenDriveId() << std::endl;
+    // std::cout << std::endl;
+
+    std::cout << "---------------------------------------------------------------------" << std::endl << std::endl;
 
     // BEGIN inserting StoppingPoints
 
